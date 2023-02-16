f6edab35f3f2208d14c4caba454b44758db5682d
Aimeana100 <aimeanathole@gmail.com>
2023-02-16 10:30:58 +0200

    ft(add-register-page) : add the register page UI
    This is the UI that a user without an account should interact with to create one.
    
    - It accept the new account creatation with an email address, name and recognizable password.

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,80 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
+import './register.scss';
 
 const Register = () => {
+  const [registerError, setRegisterError] = useState('');
   return (
-    <div>
-      Register Page
-    </div>
+    <main>
+    <section id="login">
+      <div className="container login__container">
+        <div className="login__form__container">
+          <div className="heading container">
+            <h2>Register</h2>
+          </div>
+
+          <div className="login__with__google">
+            <img src="assets/images/login_google.svg" alt="" />
+          </div>
+
+          <form action="" name="register__form" id="register__form">
+            <div className="form__row">
+              <label for="names">Names</label>
+              <input onkeyup="return validateName()" type="text" name="names" id="names"  placeholder="" />
+              <label for="names" className="error_down" id="name__error"></label>
+
+            </div>
+
+            <div className="form__row">
+              <label for="email">Email</label>
+              <input onkeyup="validateEmail()" type="text" name="email" id="email" placeholder="" />
+              <label for="email" className="error_down" id="email__error"></label>
+
+            </div>
+
+            <div className="form__row">
+              <label for="password"> Passowrd </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder=""
+              />
+              <label for="password" className="error_down" id="password__error"></label>
+            </div>
+
+            <div className="form__row">
+              <label for="confirm__password">Confirm passowrd</label>
+              <input
+                type="password"
+                name="confirm__password"
+                id="confirm__password"
+                placeholder=""
+              />
+            </div>
+
+            <div className="action__message form__row">
+              {
+                registerError && <p className="registerError">{registerError}</p>
+
+              }
+            </div>
+
+            <div className="send">
+              <button className="send" type="submit">Register</button>
+            </div>
+
+            <div className="new__register">
+              <p>
+                Alredy have an account ? 
+                <Link className='toogle__auth_option' to='/login'>Login</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  </main>
   )
 }
 
