c9451cfda101bb6ea0c367e5f0c6d5643a366d3f
Aimeana100 <aimeanathole@gmail.com>
2023-02-16 08:14:45 +0200

    ch(initialize-app) : set app the react application
    Setting up the application `my-portifolio` implemented in reactjs
    
    - initialize the react application usning npm `npx create-create-app react-portifolio`
    - install the rerevant app dependencies including:
    -   `react-icons`
    -   `react-router-dom`
    - setting up the appropliate file structure.
    - create required pages and components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+const Blog = lazy(() => import('./pages/blog/Blog'));
+const Blogs = lazy(() => import('./pages/blogs/Blogs'));
+const Login = lazy(() => import('./pages/login/Login'));
+const Register = lazy(() => import('./pages/register/Register'));
+const Home = lazy(() => import('./pages/home/Home'));
+const Header = lazy(() => import('./components/Header'));
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />  
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Suspense>
+  </Router>
+);
 
 export default App;
